73c10b6d52ad71c76d1c7e5decde7f1dba69cedf
조예진 <orobos654@gmail.com>
2023-07-29 15:19:39 +0900

    feat: init server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello world');
+import koa from 'koa';
+import Router from '@koa/router';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+
+const PORT = process.env.PORT || 8888;
+const HOST = process.env.HOST || 'localhost';
+
+const app = new koa();
+const router = new Router();
+
+router.get('/', (ctx) => {
+  ctx.response.body = 'hi there';
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
