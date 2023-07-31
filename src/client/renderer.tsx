121cedc0be99541860e7d46aa80aa0c070492564
조예진 <orobos654@gmail.com>
2023-07-31 23:48:36 +0900

    fix: set title nullable
    - todo: zod parsing 방식 고민해보기

--- src/client/renderer.tsx
+++ src/client/renderer.tsx
@@ -10,8 +10,9 @@ export const renderer = ({
   stylesheetUrl: string;
   store: Store;
 }) => {
-  const content = renderToString(<App store={store} />);
-  return `
+  try {
+    const content = renderToString(<App store={store} />);
+    return `
     <html lang="ko">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
@@ -24,4 +25,19 @@ export const renderer = ({
       </body>
     </html>
     `;
+  } catch (e) {
+    console.error(e);
+    return `<html lang="ko">
+      <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+              <title>반다라트</title>
+              <link rel="stylesheet" as="style" crossOrigin
+                    href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/static/pretendard.css"/>
+              <link rel="stylesheet" href="${stylesheetUrl}"/>
+      </head>
+      <body>
+      <div id="root">에러가 발생했어요!!</div>
+      </body>
+      </html>`;
+  }
 };
--- src/server/route/viewRoute.ts
+++ src/server/route/viewRoute.ts
@@ -8,17 +8,21 @@ import { createStore } from '../../client/stores/createStore';
 const viewRouter = new Router();
 
 viewRouter.get('/share/:key', async (ctx) => {
-  initApiClient();
-  const key = ctx.params.key;
-  const bandalartDetail = await getBandalartDetailByKey(key);
-  const bandalartCells = await getBandalartCells(key);
-  ctx.response.body = renderer({
-    stylesheetUrl: process.env.STYLESHEET_URL ?? '',
-    store: createStore({
-      bandalartDetail: bandalartDetail,
-      bandalartTree: bandalartCells,
-    }),
-  });
+  try {
+    initApiClient();
+    const key = ctx.params.key;
+    const bandalartDetail = await getBandalartDetailByKey(key);
+    const bandalartCells = await getBandalartCells(key);
+    ctx.response.body = renderer({
+      stylesheetUrl: process.env.STYLESHEET_URL ?? '',
+      store: createStore({
+        bandalartDetail: bandalartDetail,
+        bandalartTree: bandalartCells,
+      }),
+    });
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 viewRouter.get('/health', (ctx) => {
